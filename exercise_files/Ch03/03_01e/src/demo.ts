90bbf8c6f2616434cf5d692f4faf2135a5b369eb
Mario Lazzari <mario.lazzari@gmail.com>
2022-12-31 15:52:38 +0100

    done chapter 3

--- exercise_files/Ch03/03_01e/src/demo.ts
+++ exercise_files/Ch03/03_01e/src/demo.ts
@@ -1,40 +1,36 @@
 type ContactName = string;
+type ContactStatus = "active" | "inactive" | "new";
+type ContactBirthDate = Date | number | string;
 
-type ContactStatus = "active" | "inactive" | "new"
-
-type ContactBirthDate = Date | number | string
-
-interface Contact  {
-    id: number;
-    name: ContactName;
-    birthDate?: ContactBirthDate;
-    status?: ContactStatus;
+interface Contact {
+  id: number;
+  name: ContactName;
+  birthDate?: ContactBirthDate;
+  status?: ContactStatus;
 }
 
 interface Address {
-    line1: string;
-    line2: string;
-    province: string;
-    region: string;
-    postalCode: string;
+  line1: string;
+  line2: string;
+  province: string;
+  region: string;
+  postalCode: string;
 }
 
-type AddressableContact = Contact & Address
+type AddressableContact = Contact & Address;
 
 function getBirthDate(contact: Contact) {
-    if (typeof contact.birthDate === "number") {
-        return new Date(contact.birthDate);
-    }
-    else if (typeof contact.birthDate === "string") {
-        return Date.parse(contact.birthDate)
-    }
-    else {
-        return contact.birthDate
-    }
+  if (typeof contact.birthDate === "number") {
+    return new Date(contact.birthDate);
+  } else if (typeof contact.birthDate === "string") {
+    return Date.parse(contact.birthDate);
+  } else {
+    return contact.birthDate;
+  }
 }
 
-let primaryContact: Contact = {
-    id: 12345,
-    name: "Jamie Johnson",
-    status: "active"
-}
+const primaryContact: Contact = {
+  id: 12345,
+  name: "Jamie Johnson",
+  status: "active",
+};
